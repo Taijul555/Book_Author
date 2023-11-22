b458c7568b5bf1f95317868968d2d137f166352b
Taijul555 <taijulislambu@gmail.com>
2023-11-22 22:25:04 +0600

    Ado

---
+++ Books/ConnectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    public static class ConnectionHelper
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                string db = Path.Combine(Path.GetFullPath(@"..\..\"), "book_db.mdf");
+                return $@"Data Source=(localdb)\mssqllocaldb;AttachDbFilename={db};Initial Catalog=book_db;Trusted_Connection=True";
+            }
+        }
+    }
+}
